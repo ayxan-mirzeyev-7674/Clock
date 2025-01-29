e7812a6269437e8573b0107a30c7159cde17e80f
ayxan-mirzeyev-7674 <64847501+ayxan-mirzeyev-7674@users.noreply.github.com>
2025-01-29 21:53:25 +0400

    Clock

---
+++ script.js
@@ -0,0 +1,66 @@
+let clock = document.getElementById("clock");
+
+console.log(clock.offsetHeight);
+
+setInterval(() => {
+  const time = new Date();
+
+  clock.innerHTML = "";
+
+  let radius = 150;
+
+  for (let index = 0; index < radius; index += 15) {
+    let number = document.createElement("span");
+    number.innerHTML = time.getSeconds();
+    number.style.top =
+      clock.offsetHeight / 2 -
+      index * Math.sin(toRadians(6 * time.getSeconds() + 90)) +
+      "px";
+    number.style.left =
+      clock.offsetWidth / 2 -
+      index * Math.cos(toRadians(6 * time.getSeconds() + 90)) +
+      "px";
+    number.classList.add("second");
+    clock.appendChild(number);
+  }
+
+  radius = 130;
+
+  for (let index = 0; index < radius; index += 15) {
+    let number = document.createElement("span");
+    number.innerHTML = time.getMinutes();
+    number.style.top =
+      clock.offsetHeight / 2 -
+      index * Math.sin(toRadians(6 * time.getMinutes() + 90)) +
+      "px";
+    number.style.left =
+      clock.offsetWidth / 2 -
+      index * Math.cos(toRadians(6 * time.getMinutes() + 90)) +
+      "px";
+    number.classList.add("minute");
+
+    clock.appendChild(number);
+  }
+
+  radius = 100;
+
+  for (let index = 0; index < radius; index += 15) {
+    let number = document.createElement("span");
+    number.innerHTML = time.getHours();
+    number.style.top =
+      clock.offsetHeight / 2 -
+      index * Math.sin(toRadians(30 * time.getHours() + 90)) +
+      "px";
+    number.style.left =
+      clock.offsetWidth / 2 -
+      index * Math.cos(toRadians(30 * time.getHours() + 90)) +
+      "px";
+    number.classList.add("hour");
+
+    clock.appendChild(number);
+  }
+}, 1000);
+
+function toRadians(angle) {
+  return angle * (Math.PI / 180);
+}
